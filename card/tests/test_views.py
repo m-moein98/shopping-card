f9fdce9a213cfc26c01091616530392248c4086c
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2022-01-18 20:35:16 +0330

    initialize card tests

--- card/tests/test_views.py
+++ card/tests/test_views.py
@@ -2,4 +2,14 @@ from .test_setup import TestSetUp
 
 
 class TestViews(TestSetUp):
-    pass
+    def test_user_can_get_items_in_card(self):
+        pass
+
+    def test_user_can_add_item_to_card(self):
+        pass
+
+    def test_user_can_update_item_in_card(self):
+        pass
+
+    def test_user_can_remove_item_from_card(self):
+        pass
