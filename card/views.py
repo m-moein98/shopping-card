65d98eb5feb4b0ae7af12e7aa6161e36c7eb64ef
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2022-01-19 12:12:29 +0330

    add card views based on unit tests

--- card/views.py
+++ card/views.py
@@ -1,13 +1,27 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Card
+from .serializers import CardSerializer
 
 
 class CardView(APIView):
     def get(self, request):
-        return Response()
+        query = Card.objects.all()
+        serializer = CardSerializer(query, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
-        return Response()
-    def put(self, request):
-        return Response()
-    def delete(self, request):
-        return Response()
+        name = request.data['name']
+        query = Card.objects.create(name=name)
+        return Response(f"card created : {query.name}")
+
+    def put(self, request, id):
+        name = request.data['name']
+        query = Card.objects.filter(id=id).update(name=name)
+
+        return Response(f"card updated :{name}", status=status.HTTP_202_ACCEPTED)
+
+    def delete(self, request, id):
+        query = Card.objects.filter(id=id).delete()
+        return Response(f"card deleted")
