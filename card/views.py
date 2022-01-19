541e0691b092cb424fd11917ea15b0b9559d2f65
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2022-01-19 12:17:09 +0330

    add exception handling system

--- card/views.py
+++ card/views.py
@@ -1,27 +1,42 @@
+from logging import exception
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+
 from .models import Card
 from .serializers import CardSerializer
 
 
 class CardView(APIView):
     def get(self, request):
-        query = Card.objects.all()
-        serializer = CardSerializer(query, many=True)
-        return Response(serializer.data)
+        try:
+            query = Card.objects.all()
+            serializer = CardSerializer(query, many=True)
+            return Response(serializer.data)
+        except exception as e:
+            return Response(str(e))
 
     def post(self, request):
-        name = request.data['name']
-        query = Card.objects.create(name=name)
-        return Response(f"card created : {query.name}")
+        try:
+            name = request.data['name']
+            query = Card.objects.create(name=name)
+            return Response(f"card created : {query.name}")
+        except exception as e:
+            return Response(str(e))
 
     def put(self, request, id):
-        name = request.data['name']
-        query = Card.objects.filter(id=id).update(name=name)
+        try:
+            name = request.data['name']
+            query = Card.objects.filter(id=id).update(name=name)
 
-        return Response(f"card updated :{name}", status=status.HTTP_202_ACCEPTED)
+            return Response(f"card updated :{name}", status=status.HTTP_202_ACCEPTED)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, id):
-        query = Card.objects.filter(id=id).delete()
-        return Response(f"card deleted")
+        try:
+            Card.objects.filter(id=id).delete()
+            return Response(f"card deleted")
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
